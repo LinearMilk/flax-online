7157ff6cf7c14d57e4842abd322786a05b0092df
Luara Neumann <luaralulu@gmail.com>
2018-03-04 13:14:49 +0100

    refactoring and displaying tiles from getRandomTile()

--- scripts/player.js
+++ scripts/player.js
@@ -6,9 +6,13 @@ class Player {
 		this.tileSupply = [8,8,8];
 	}
 
+	getColour(){
+		return this.colour;
+	}
+
 	getRandomTile(){
 		if(this.tileSupply[0] + this.tileSupply[1] + this.tileSupply[2] === 0){
-			return -1;
+			return [];
 		}
 
 		var tile = Math.floor(Math.random() * Math.floor(3));
@@ -19,7 +23,23 @@ class Player {
 			tile = this.getRandomTile();
 		}
 
-		return tile;
+		return this.getRandomTileOptions(tile);
+	}
+
+	getRandomTileOptions(tile){
+		switch(tile){
+			case 0:
+				return [1,6];
+				break;
+			case 1:
+				return [2,5];
+				break;
+			case 2:
+				return [3,4];
+				break;
+			default:
+				break;
+		}
 	}
 
 }
