55b9de86eabdb02894a3cf9eb38c02024055072c
LinearMilk <witold.henszel@gmail.com>
2018-03-09 10:31:12 +0100

    Removed variables.js and moved global variables into main.js for later refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,21 @@
-var game = new Game();
+const canvasId = "game-canvas";
+const gameBoardHeight = 9;
+const squareSize = 46;
+const gameBoardFrameSize = 10;
+
+const squareBorderSize = 1;
+const gameBoardWidth = 13;
+
+const playerColours = ["#f9cb32","#90b531","#ed4630","#dc6fe6"];
+
+const startingPosition = [1,9];
+const startingColour = playerColours[3];
+
+let xClick = -1;
+let yClick = -1;
+
+const game = new Game();
+
 
 function startGame(){
 	game.startGame();
