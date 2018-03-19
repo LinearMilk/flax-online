6a11f8769967c0a3cb035bdb7056e1b5fd4a22e6
Luara Neumann <luaralulu@gmail.com>
2018-03-19 19:24:33 +0100

    Adding one more test
    issue with prettier trying to fix the files

--- tests/findSquare.test.js
+++ tests/findSquare.test.js
@@ -1,6 +1,7 @@
 import GameEngineChipMoves from "../scripts/gameEngineChipMoves";
 import Chip from "../scripts/chip";
 
+const chip = new Chip("white", 1, [1, 1]);
 const squares = [
   {
     xCoordinate: 1,
@@ -37,21 +38,23 @@ const squares = [
 ];
 
 test("chip null should return no square (null)", () => {
-  expect(GameEngineChipMoves.findSquare(squares, null)).toBe(null);
+  expect(GameEngineChipMoves.findSquare(squares, null)).toBe(undefined);
 });
 
 test("empty squares should return no square (null)", () => {
-  const chip = new Chip("white", 1, [1, 1]);
-  expect(GameEngineChipMoves.findSquare([], chip)).toBe(null);
+  expect(GameEngineChipMoves.findSquare([], chip)).toBe(undefined);
 });
 
 test("squares=null should return no square (null)", () => {
-  const chip = new Chip("white", 1, [1, 1]);
-  expect(GameEngineChipMoves.findSquare(null, chip)).toBe(null);
+  expect(GameEngineChipMoves.findSquare(null, chip)).toBe(undefined);
 });
 
 test("chip on [1,1] should return square on [1,1]", () => {
-  const chip = new Chip("white", 1, [1, 1]);
   squares[0].activeChip = chip;
   expect(GameEngineChipMoves.findSquare(squares, chip)).toBe(squares[0]);
+});
+
+test("chip on [2,2] that is not in the board should return no square", () => {
+  const chip = new Chip("white", 1, [2, 2]);
+  expect(GameEngineChipMoves.findSquare(squares, chip)).toBe(undefined);
 });
