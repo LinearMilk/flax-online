d66bb3b9afe440617958c0a7bd4571dce45df993
LinearMilk <witold.henszel@gmail.com>
2018-03-28 18:07:39 +0200

    Adding happy path for chip value 1 for findNearestChip

--- tests/findNearestChip.test.js
+++ tests/findNearestChip.test.js
@@ -2,6 +2,10 @@ import GameEngineChipMoves from "../scripts/gameEngineChipMoves";
 import Chip from "../scripts/chip";
 import * as testConstants from "./testConstants";
 
+beforeEach(() => {
+  testConstants.clearChipsFromSquares();
+});
+
 test("chip null should return no square (undefined)", () => {
   expect(GameEngineChipMoves.findNearestChip(testConstants.squares, null, "West")).toBe(undefined);
 });
@@ -22,16 +26,50 @@ test("any chip on [1,1] for North should return outside of board (undefined)", (
   expect(GameEngineChipMoves.findNearestChip(testConstants.squares, testConstants.chipOne, "North")).toBe(undefined);
 });
 
+test("any chip on [1,1] for West should return outside of board (undefined)", () => {
+  expect(GameEngineChipMoves.findNearestChip(testConstants.squares, testConstants.chipOne, "West")).toBe(undefined);
+});
+
+test("any chip on [2,2] for South should return outside of board (undefined) for 2x2 board", () => {
+  expect(GameEngineChipMoves.findNearestChip(testConstants.squares, testConstants.chipThree, "South")).toBe(undefined);
+});
+
+test("any chip on [2,2] for East should return outside of board (undefined) for 2x2 board", () => {
+  expect(GameEngineChipMoves.findNearestChip(testConstants.squares, testConstants.chipThree, "East")).toBe(undefined);
+});
+
 test("chip on [2,2] that is not in the board should return no square (undefined)", () => {
-  const chip = new Chip("white", 1, [2, 2]);
-  expect(GameEngineChipMoves.findNearestChip(testConstants.squares, chip, "South")).toBe(undefined);
+  expect(GameEngineChipMoves.findNearestChip(testConstants.squares, testConstants.chipThree, "South")).toBe(undefined);
 });
 
 test("chip(1) on [2,2] with chip on the board at [2,1] should return square [2,1] for North", () => {
-  const chip = new Chip("white", 1, [2, 2]);
   const existingChipLocation = 2;
   testConstants.squares[existingChipLocation].activeChip = testConstants.chipTwo;
-  expect(GameEngineChipMoves.findNearestChipNorth(testConstants.squares, testConstants.chipThree)).toEqual(
+  expect(GameEngineChipMoves.findNearestChip(testConstants.squares, testConstants.chipThree, "North")).toEqual(
+    testConstants.squares[existingChipLocation]
+  );
+});
+
+test("chip(1) on [2,1] with chip on the board at [1,1] should return square [1,1] for East", () => {
+  const existingChipLocation = 0;
+  testConstants.squares[existingChipLocation].activeChip = testConstants.chipOne;
+  expect(GameEngineChipMoves.findNearestChip(testConstants.squares, testConstants.chipTwo, "East")).toEqual(
+    testConstants.squares[existingChipLocation]
+  );
+});
+
+test("chip(1) on [1,1] with chip on the board at [1,2] should return square [1,2] for South", () => {
+  const existingChipLocation = 1;
+  testConstants.squares[existingChipLocation].activeChip = testConstants.chipSix;
+  expect(GameEngineChipMoves.findNearestChip(testConstants.squares, testConstants.chipOne, "South")).toEqual(
+    testConstants.squares[existingChipLocation]
+  );
+});
+
+test("chip(1) on [2,2] with chip on the board at [1,2] should return square [1,2] for West", () => {
+  const existingChipLocation = 1;
+  testConstants.squares[existingChipLocation].activeChip = testConstants.chipSix;
+  expect(GameEngineChipMoves.findNearestChip(testConstants.squares, testConstants.chipThree, "West")).toEqual(
     testConstants.squares[existingChipLocation]
   );
 });
