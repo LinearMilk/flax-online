d470577b712dc7e8843185a279f0d7cee23f834e
LinearMilk <witold.henszel@gmail.com>
2018-03-29 12:23:45 +0200

    Adding tests for chipMovesEast

--- scripts/gameEngineChipMoves.js
+++ scripts/gameEngineChipMoves.js
@@ -132,7 +132,7 @@ export default class GameEngineChipMoves {
    */
   static checkMovesEast(chipToBeChecked, squareWithChip) {
     const chip = chipToBeChecked;
-
+    if (!chip || !squareWithChip) return undefined;
     const possibleMoves = chip.validMoves;
     if (squareWithChip) {
       const foundChip = squareWithChip.activeChip;
