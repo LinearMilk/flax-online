afc70450f08a125200540a2bcfe04afef2a73d8f
LinearMilk <witold.henszel@gmail.com>
2018-03-31 13:06:25 +0200

    Adding all the tests

---
+++ tests/findSquare.test.js
@@ -0,0 +1,29 @@
+import GameEngineChipMoves from "../scripts/gameEngineChipMoves";
+import Chip from "../scripts/chip";
+import * as testConstants from "./testConstants";
+
+beforeEach(() => {
+  testConstants.clearChipsFromSquares();
+});
+
+test("chip null should return no square (undefined)", () => {
+  expect(GameEngineChipMoves.findSquare(testConstants.squares, null)).toBe(undefined);
+});
+
+test("empty squares should return no square (undefined)", () => {
+  expect(GameEngineChipMoves.findSquare([], testConstants.chipOne)).toBe(undefined);
+});
+
+test("squares=null should return no square (undefined)", () => {
+  expect(GameEngineChipMoves.findSquare(null, testConstants.chipOne)).toBe(undefined);
+});
+
+test("chip on [1,1] should return square on [1,1]", () => {
+  testConstants.squares[0].activeChip = testConstants.chipOne;
+  expect(GameEngineChipMoves.findSquare(testConstants.squares, testConstants.chipOne)).toBe(testConstants.squares[0]);
+});
+
+test("chip on [2,2] that is not in the board should return no square (undefined)", () => {
+  const chip = new Chip("white", 1, [2, 2]);
+  expect(GameEngineChipMoves.findSquare(testConstants.squares, chip)).toBe(undefined);
+});
---
+++ tests/testConstants.js
@@ -0,0 +1,65 @@
+import Chip from "../scripts/chip";
+
+const chipOne = new Chip("white", 1, [1, 1]);
+const chipTwo = new Chip("white", 1, [2, 1]);
+const chipThree = new Chip("white", 1, [2, 2]);
+const chipFour = new Chip("white", 4, [3, 3]);
+const chipFive = new Chip("white", 4, [5, 6]);
+const chipSix = new Chip("white", 1, [1, 2]);
+const chipSeven = new Chip("red", 2, [1, 2]);
+const chipEight = new Chip("red", 2, [2, 1]);
+
+const squares = [
+  {
+    xCoordinate: 1,
+    yCoordinate: 1,
+    roomNumber: 0,
+    activeChip: null,
+    bottomChip: null,
+    startingTile: ""
+  },
+  {
+    xCoordinate: 1,
+    yCoordinate: 2,
+    roomNumber: 0,
+    activeChip: null,
+    bottomChip: null,
+    startingTile: ""
+  },
+  {
+    xCoordinate: 2,
+    yCoordinate: 1,
+    roomNumber: 0,
+    activeChip: null,
+    bottomChip: null,
+    startingTile: ""
+  },
+  {
+    xCoordinate: 2,
+    yCoordinate: 2,
+    roomNumber: 0,
+    activeChip: null,
+    bottomChip: null,
+    startingTile: ""
+  }
+];
+
+function clearChipsFromSquares() {
+  squares.forEach(square => {
+    square.activeChip = null;
+    square.bottomChip = null;
+  });
+}
+
+export {
+  chipOne,
+  chipTwo,
+  chipThree,
+  chipFour,
+  chipFive,
+  chipSix,
+  chipSeven,
+  chipEight,
+  squares,
+  clearChipsFromSquares
+};
