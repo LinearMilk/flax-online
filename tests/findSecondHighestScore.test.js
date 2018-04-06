6227f5322a67ae16e2a9b4afd625a53a4ecd5d23
LinearMilk <witold.henszel@gmail.com>
2018-04-06 10:19:46 +0200

    Adding happy path tests for findSecondHighestScore

--- tests/findSecondHighestScore.test.js
+++ tests/findSecondHighestScore.test.js
@@ -28,3 +28,59 @@ test("no pips should return -1 (3 players)", () => {
 test("all equal pip counts should return -1 (3 players)", () => {
   expect(GameEngine.findSecondHighestScore([10, 10, 10])).toEqual(-1);
 });
+
+test("[0,5,0] should return -1 (3 players)", () => {
+  expect(GameEngine.findSecondHighestScore([0, 5, 0])).toEqual(-1);
+});
+
+test("[0,6,6] should return -1 (3 players)", () => {
+  expect(GameEngine.findSecondHighestScore([0, 6, 6])).toEqual(-1);
+});
+
+test("[7,0,0,7] should return -1 (4 players)", () => {
+  expect(GameEngine.findSecondHighestScore([7, 0, 0, 7])).toEqual(-1);
+});
+
+test("[9,9,9,0] should return -1 (4 players)", () => {
+  expect(GameEngine.findSecondHighestScore([9, 9, 9, 0])).toEqual(-1);
+});
+
+test("[2,1] should return [0] (2 players)", () => {
+  expect(GameEngine.findSecondHighestScore([2, 1])).toEqual([0]);
+});
+
+test("[2,4] should return [1] (2 players)", () => {
+  expect(GameEngine.findSecondHighestScore([2, 4])).toEqual([1]);
+});
+
+test("[2,6,6] should return [0] (3 players)", () => {
+  expect(GameEngine.findSecondHighestScore([2, 6, 6])).toEqual([0]);
+});
+
+test("[2,4,6] should return [1] (3 players)", () => {
+  expect(GameEngine.findSecondHighestScore([2, 4, 6])).toEqual([1]);
+});
+
+test("[0,6,3] should return [2] (3 players)", () => {
+  expect(GameEngine.findSecondHighestScore([0, 6, 3])).toEqual([2]);
+});
+
+test("[3,6,3] should return [0,2] (3 players)", () => {
+  expect(GameEngine.findSecondHighestScore([3, 6, 3])).toEqual([0, 2]);
+});
+
+test("[1,9,9,0] should return [0] (4 players)", () => {
+  expect(GameEngine.findSecondHighestScore([1, 9, 9, 0])).toEqual([0]);
+});
+
+test("[1,9,9,0] should return [0] (4 players)", () => {
+  expect(GameEngine.findSecondHighestScore([1, 9, 9, 0])).toEqual([0]);
+});
+
+test("[3,9,3,3] should return [0,2,3] (4 players)", () => {
+  expect(GameEngine.findSecondHighestScore([3, 9, 3, 3])).toEqual([0, 2, 3]);
+});
+
+test("[4,2,3,1] should return [2] (4 players)", () => {
+  expect(GameEngine.findSecondHighestScore([4, 2, 3, 1])).toEqual([2]);
+});
